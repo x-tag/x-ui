f7c25ce93af774d415325b3b990e7ae7005c3a1e
Arron Schaar <aschaar@gmail.com>
2013-03-21 17:11:30 -0700

    added stub files for stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,27 +8,54 @@ module.exports = function(grunt) {
     'smush-components': {
       options: {
         fileMap: {
-          js: 'demo/x-tag-components.js',
-          css: 'demo/x-tag-components.css'
+          js: 'ui/x-tag-components.js',
+          css: 'ui/x-tag-components.css'
         }
       }
     },
+    /*watch: {
+      stylus: {
+        files: ['src/styl/*'],
+        tasks: ['stylus']
+      }
+    },*/
     bumpup: ['component.json', 'package.json', 'xtag.json'],
     tagrelease: {
       file: 'package.json',
       prefix: 'xtag-v',
       commit: true
+    }, 
+    stylus:{     
+      light: {
+        options:{
+          define: {
+            theme: 'light'
+          },
+        },
+        files:{
+          'src/light.css': ['src/styl/main.styl']
+        }
+      },
+      dark: {
+        options:{
+          define: {
+            theme: 'dark'
+          },
+        },
+        files: {
+          'src/dark.css': ['src/styl/main.styl']
+        }
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-bumpup');
   grunt.loadNpmTasks('grunt-tagrelease');
   grunt.loadNpmTasks('grunt-smush-components');
 
-  
-  grunt.registerTask('build', ['jshint','smush-components']);
+  grunt.registerTask('build', ['smush-components','stylus']);
   grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
 
 
