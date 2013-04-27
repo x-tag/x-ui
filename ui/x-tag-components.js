e6add99b7b217013195eea5e6aa7982cfbe9d92d
Arron Schaar <aschaar@gmail.com>
2013-04-26 17:07:29 -0700

    added flipbox, shiftbox and x-switch

--- ui/x-tag-components.js
+++ ui/x-tag-components.js
@@ -723,6 +723,42 @@ if (!(document.register || {}).__polyfill__){
 
 })();
 
+(function(){  
+
+  xtag.register('x-appbar', {
+    lifecycle: {
+      created: function(){
+        var header = xtag.queryChildren(this, 'header')[0];
+        if (!header){          
+          header = document.createElement('header');          
+          this.appendChild(growbox);
+        }        
+        this.xtag.data.header = header;
+        this.subheading = this.subheading;
+      }
+    },
+    accessors: {
+      heading: {
+        get: function(){
+          return this.xtag.data.header.innerHTML;
+        },
+        set: function(value){
+          this.xtag.data.header.innerHTML = value;        
+        }
+      }, 
+      subheading: {
+        get: function(){
+          return this.getAttribute('subheading') || "";
+        },
+        'set:attribute': function(value){
+          this.xtag.data.header.setAttribute('subheading', value);
+        }
+      }
+    }
+  });
+
+})();
+
 (function() {
 
   var changeFlipDirection = function(elem, dir) {
@@ -802,4 +838,78 @@ if (!(document.register || {}).__polyfill__){
 		}
 	});
 	
+})();
+(function(){
+  
+  var template =  '<input type="checkbox" />' +
+  '<div>' +
+    '<div class="x-switch-text" ontext="ON" offtext="OFF"></div>' +
+    '<div><div class="x-switch-knob"><br/></div></div>' +
+    '<div class="x-switch-knob">' +
+      '<div class="x-switch-background">' +
+        '<div class="x-switch-text x-switch-on" ontext="ON" offtext="OFF"></div>' +
+        '<div><div class="x-switch-knob"><br/></div></div>' +
+        '<div class="x-switch-text x-switch-off" ontext="ON" offtext="OFF"></div>' +
+      '</div>' +
+    '</div>' +
+  '</div>';
+
+  function textSetter(state){
+    var obj = {
+      get: function(){
+        return this.getAttribute(state + 'text') || state;
+      }
+    };
+    obj['set:attribute(' + state + 'text)'] = function(text){
+      xtag.query(this, '[' + state + 'text]').forEach(function(el){
+        el.setAttribute(state + 'text', text);
+      });
+    }
+    return obj;
+  }
+  
+  xtag.register('x-switch', {
+    lifecycle: {
+      created: function(){
+        this.innerHTML = template;
+        this.onText = this.onText;
+        this.offText = this.offText;
+        this.checked = this.checked;
+        this.formName = this.formName;
+      }
+    },
+    methods: {
+      toggle: function(state){
+        this.checked = typeof state == 'undefined' ? (this.checked ? false : true) : state;
+      }
+    },
+    events:{
+      'change': function(e){
+        e.target.focus();
+        this.checked = this.checked;
+      }
+    },
+    accessors: {
+      onText: textSetter('on'),
+      offText: textSetter('off'),
+      checked: {
+        get: function(){
+          return this.firstElementChild.checked;
+        },
+        'set:attribute': function(state){
+          this.firstElementChild.checked = state;
+          this.firstElementChild.checked ? this.removeAttribute('checked') : this.setAttribute('checked', null);
+        } 
+      },
+      formName: {
+        get: function(){
+          return this.firstElementChild.getAttribute('name') || this.getAttribute('formName');
+        },
+        'set:attribute(formname)': function(value){
+          this.firstElementChild.setAttribute('name', value);
+        }
+      }
+    }
+  });
+
 })();
